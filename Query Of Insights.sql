90f20301e1d2318774d45ba2f2253a4cd826f73d
Soumya-De <soumyadey820@gmail.com>
2025-07-07 10:53:33 +0530

    Add sales and revenue analysis queries
    Added multiple SQL queries to analyze pizza sales by size, category, revenue, quantity, and order count. Includes top and bottom 5 pizzas by revenue, quantity sold, and number of orders, as well as percentage of sales by size and total sold by category.

--- Query Of Insights.sql
+++ Query Of Insights.sql
@@ -20,6 +20,65 @@ JOIN pizza_prices pp ON po.pizza_name_id = pp.pizza_name_id
 JOIN pizza_info pi ON pp.pizza_name = pi.pizza_name
 GROUP BY pi.pizza_category;
 
+SELECT pp.pizza_size,
+ROUND((SUM(po.total_price) / (SELECT SUM(total_price) FROM pizza_orders)) * 100,2) 
+AS percentage_of_sales
+FROM pizza_orders po
+JOIN pizza_prices pp ON po.pizza_name_id = pp.pizza_name_id
+GROUP BY pp.pizza_size;
 
+SELECT pi.pizza_category,
+sum(po.quantity) 
+AS total_sold
+FROM pizza_orders po
+JOIN pizza_prices pp ON po.pizza_name_id = pp.pizza_name_id
+JOIN pizza_info pi ON pp.pizza_name = pi.pizza_name
+GROUP BY pi.pizza_category;
 
+SELECT pp.pizza_name,
+round(sum(po.total_price),2)
+AS total_revenue
+FROM pizza_orders po
+JOIN pizza_prices pp ON po.pizza_name_id = pp.pizza_name_id
+GROUP BY pp.pizza_name
+order by total_revenue desc limit 5;
 
+SELECT pp.pizza_name,
+round(sum(po.total_price),2)
+AS total_revenue
+FROM pizza_orders po
+JOIN pizza_prices pp ON po.pizza_name_id = pp.pizza_name_id
+GROUP BY pp.pizza_name
+order by total_revenue limit 5;
+
+SELECT pp.pizza_name,
+(sum(po.quantity)
+AS total_quantity
+FROM pizza_orders po
+JOIN pizza_prices pp ON po.pizza_name_id = pp.pizza_name_id
+GROUP BY pp.pizza_name
+order by total_quantity desc limit 5;
+
+SELECT pp.pizza_name,
+(sum(po.quantity)
+AS total_quantity
+FROM pizza_orders po
+JOIN pizza_prices pp ON po.pizza_name_id = pp.pizza_name_id
+GROUP BY pp.pizza_name
+order by total_quantity limit 5;
+
+SELECT pp.pizza_name,
+sum(distinct po.order_id)
+AS total_orders
+FROM pizza_orders po
+JOIN pizza_prices pp ON po.pizza_name_id = pp.pizza_name_id
+GROUP BY pp.pizza_name
+order by total_orders desc limit 5;
+
+SELECT pp.pizza_name,
+sum(distinct po.order_id)
+AS total_orders
+FROM pizza_orders po
+JOIN pizza_prices pp ON po.pizza_name_id = pp.pizza_name_id
+GROUP BY pp.pizza_name
+order by total_orders limit 5;
